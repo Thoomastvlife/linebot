fcbc2f18c36ec8f9df1f6028021ffe8aedd892ed
Thoomastvlife <153334866+Thoomastvlife@users.noreply.github.com>
2024-01-24 14:47:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
 
-app = Flask(__name__)
+app = Flask(專屬客服)
 
 line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
@@ -27,6 +27,6 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
-if __name__ == "__main__":
+if 專屬客服 == "udb23d4459532914a3c5bbd243f429a70":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
